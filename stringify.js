4a72028994da1d283c8c352237ddb11448c266ce
Julian Gruber <julian@juliangruber.com>
2013-05-01 20:25:20 +0200

    correct buffer length calculation

--- stringify.js
+++ stringify.js
@@ -6,16 +6,23 @@ module.exports = stringify;
 
 function stringify (method, key, value) {
   var keyLength = key.length;
-  var valueLength = value.length || 0;
-  var buf = new Buffer(3 + keyLength + valueLength);
+  var valueLength = value.length;
+
+  var length = 2 + keyLength;
+
+  if (method === 'put') {
+    length += 1 + valueLength
+  }
+
+  var buf = new Buffer(length);
 
   buf.writeUInt8(method == 'put');
   buf.writeUInt8(keyLength, 1);
-  buf.write(key);
+  buf.write(key, 2);
 
   if (method === 'put') {
     buf.writeUInt8(valueLength, 2 + keyLength);
-    buf.write(value);
+    buf.write(value, 2 + keyLength + 1);
   }
 
   return buf;
