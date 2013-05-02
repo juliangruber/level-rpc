1c7728bbefbccd97beb680ba813e0b1b6ce4ed8a
Julian Gruber <julian@juliangruber.com>
2013-05-02 21:02:29 +0200

    stringify buffer version

--- stringify.js
+++ stringify.js
@@ -11,7 +11,7 @@ function stringify (id, method, key, value) {
   // 6 = method + 4x callback id + key length
   var length = 6 + keyLength;
 
-  if (method === 'put') {
+  if (method == 'put') {
     valueLength = value.length;
     length += 1 + valueLength;
   }
@@ -23,10 +23,37 @@ function stringify (id, method, key, value) {
   buf.writeUInt8(keyLength, 5);
   buf.write(key, 6);
 
-  if (method === 'put') {
+  if (method == 'put') {
     buf.writeUInt8(valueLength, 6 + keyLength);
     buf.write(value, 6 + keyLength + 1);
   }
 
   return buf;
-}
+};
+
+stringify.buffer = function (id, method, key, value) {
+  var keyLength = key.length;
+  var valueLength;
+
+  // 6 = method + 4x callback id + key length
+  var length = 6 + keyLength;
+
+  if (method == 'put') {
+    valueLength = value.length;
+    length += 1 + valueLength;
+  }
+
+  var buf = new Buffer(length);
+
+  buf.writeUInt32LE(id, 0);
+  buf.writeUInt8(method == 'put' ? 1 : 0, 4);
+  buf.writeUInt8(keyLength, 5);
+  key.copy(buf, 6);
+
+  if (method == 'put') {
+    buf.writeUInt8(valueLength, 6 + keyLength);
+    value.copy(buf, 6 + keyLength + 1);
+  }
+
+  return buf;
+};
